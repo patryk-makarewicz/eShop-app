b0f4fb4fcc02af8f7f96b926cf428f18dc9199c8
Patryk Makarewicz <makarewicz.szymon@gmail.com>
2021-03-01 13:39:19 +0100

    add form from hook-form

--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-const Login = () => (
-  <form>
-    <input name="login" />
-    <input name="password" />
-  </form>
-);
+import styles from './login.module.scss';
+
+type Inputs = {
+  login: string;
+  password: string;
+};
+
+const Login = () => {
+  const { register, handleSubmit, errors } = useForm<Inputs>();
+  const onSubmit = (data: any) => console.log(data);
+
+  return (
+    <div className={styles.wrapper}>
+      <p>Login to eShop app</p>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        <input name="login" ref={register({ required: true })} placeholder="Your login" />
+        {errors.login && <span>This field is required</span>}
+        <input name="password" ref={register({ required: true })} placeholder="Your password" />
+        {errors.password && <span>This field is required</span>}
+        <input type="submit" />
+      </form>
+    </div>
+  );
+};
 
 export default Login;
